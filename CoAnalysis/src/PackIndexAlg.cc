cdd05e8fea2852559b06b1055e765e95cbc48051
zoujh <zoujh@ihep.ac.cn>
2021-11-22 10:06:50 +0800

    optimize the PackIndexAlg

--- CoAnalysis/src/PackIndexAlg.cc
+++ CoAnalysis/src/PackIndexAlg.cc
@@ -24,10 +24,10 @@ public:
 private:
     long nextIndexEntry();
     FragPtr index2Frag(long entry);
-    bool inTimeWindow(JM::EvtNavigator*p, TTimeStamp t0);
+    bool inTimeWindow(JM::EvtNavigator *p, const TTimeStamp &t0);
 
-    long m_curEntry;//记录当前读文件读到了哪个Entry,避免evt被重复读入
-    FragPtr m_lastFrag;//存放前一次放入globalbuffer的fragment
+    long m_lastEntry;  //记录前一个fragment读到了哪个Entry,避免evt被重复读入
+    FragPtr m_lastFrag;  //存放前一次放入globalbuffer的fragment
     
     GlobalBuffer<Frag> *m_gbuf;
 
@@ -44,7 +44,7 @@ DECLARE_ALGORITHM(PackIndexAlg);
 
 PackIndexAlg::PackIndexAlg(const std::string &name)
     : AlgBase(name),
-      m_curEntry(-1)
+      m_lastEntry(-1)
 {
     declProp("IndexFile", m_index);
     declProp("DataFiles", m_data);
@@ -85,93 +85,29 @@ bool PackIndexAlg::execute()
 
     //read events around the index event and generate a Fragment
     m_lastFrag = index2Frag(indexEntry);
+    //dump the fragment for debug
+    if (logLevel() <= 2)
+    {
+        using SniperLog::LogStream;
+        int i = 0;
+        SniperLog::Logger::lock();
+        *LogStream << "\t[" << std::endl;
+        for (auto& nav : m_lastFrag->evtDeque)
+        {
+            *LogStream << "\t\t" << dynamic_cast<JM::OECEvent*>(nav->getHeader("/Event/OEC")->event("JM::OECEvent"))->getTime();
+            if (i++ == m_lastFrag->lbegin) {
+                *LogStream << "\t" << "(index)";
+            }
+            *LogStream << std::endl;
+        }
+        *LogStream << "\t]" << std::endl;
+        SniperLog::Logger::unlock();
+    }
 
     //put the Fragment to GlobalBuffer
     m_gbuf->push_back(m_lastFrag);
 
     return true;
-}
-
-long PackIndexAlg::nextIndexEntry()
-{
-    long entry;
-    m_indexStream >> entry;
-    if ( ! m_indexStream.good() ) {
-        m_par->stop();
-    }
-    LogDebug << "reading event " << entry << std::endl;
-    return entry;
-}
-
-PackIndexAlg::FragPtr PackIndexAlg::index2Frag(long entry)
-{
-    PackIndexAlg::FragPtr dest(new Frag);
-    m_dataStream->setEntry(entry);
-    JM::EvtNavigator* cur = m_dataStream->get();
-
-    auto evt = dynamic_cast<JM::OECEvent*>(cur->getHeader("/Event/OEC")->event("JM::OECEvent"));
-    auto& t0 = evt->getTime();
-
-    LogDebug << t0 << std::endl;
-
-    dest->evtDeque.push_back(std::shared_ptr<JM::EvtNavigator>(cur));
-    dest->lbegin = 0;
-    dest->lend = 1;
-
-    long ientry = entry;
-    while (--ientry >= 0)
-    {   if(ientry > m_curEntry){//要读的事例在curEntry之后，说明不曾被放入过fragment
-            m_dataStream->prev();
-            auto p = m_dataStream->get();
-            if (!inTimeWindow(p, t0))
-            {
-                delete p;
-                break;
-            }
-            dest->evtDeque.push_front(std::shared_ptr<JM::EvtNavigator>(p));
-            ++(dest->lbegin);
-            ++(dest->lend);
-        }
-
-        if(ientry <= m_curEntry){//读入的事例曾被放入过fragment
-            auto _i = m_lastFrag->evtDeque.size()-1 - (m_curEntry - ientry);//要取的evt在lastFrag的位置
-            
-            if(_i < 0) break;//说明向前读的事例已经超出了上一个frag的第0个
-            
-            auto _nav = m_lastFrag->evtDeque[_i];
-            if(!inTimeWindow(_nav.get(), t0)) break;
-
-            dest->evtDeque.push_front(_nav);
-        }
-
-    }
-
-    ientry = entry;
-    while (++ientry < m_total)
-    {   if(ientry <= m_curEntry){//要读的事例在curEntry之前，需要从lastFrag取
-            auto _i = m_lastFrag->evtDeque.size()-1 - (m_curEntry - ientry);
-
-            auto _nav = m_lastFrag->evtDeque[_i];
-            if(!inTimeWindow(_nav.get(), t0)) break;
-
-            dest->evtDeque.push_back(_nav);
-        }
-        
-        
-        if(ientry > m_curEntry){//要读的事例在curEntry之后,说明不曾被放入过frag
-            m_dataStream->next();
-            auto p = m_dataStream->get();
-            if (!inTimeWindow(p, t0))
-            {
-                delete p;
-                m_curEntry = ientry-1;//本次pack结束后更新m_curEntry
-                break;
-            }
-            dest->evtDeque.push_back(std::shared_ptr<JM::EvtNavigator>(p));
-        }
-    }
-
-    return dest;
 }
 
 bool PackIndexAlg::finalize()
@@ -186,13 +122,96 @@ bool PackIndexAlg::finalize()
     return true;
 }
 
+long PackIndexAlg::nextIndexEntry()
+{
+    long entry;
+    m_indexStream >> entry;
+    if ( ! m_indexStream.good() ) {
+        m_par->stop();
+    }
+    return entry;
+}
 
-bool PackIndexAlg::inTimeWindow(JM::EvtNavigator*p, TTimeStamp t0){
+PackIndexAlg::FragPtr PackIndexAlg::index2Frag(long entry)
+{
+    PackIndexAlg::FragPtr dest(new Frag);
+
+    std::shared_ptr<JM::EvtNavigator> cur;
+    if (entry > m_lastEntry)
+    {
+        m_dataStream->setEntry(entry);
+        cur.reset(m_dataStream->get());
+    }
+    else
+    {
+        auto _it = m_lastFrag->evtDeque.rbegin() + (m_lastEntry - entry); //要取的evt在lastFrag的位置
+        cur = *_it;
+    }
+    dest->evtDeque.push_back(cur);
+
+    auto evt = dynamic_cast<JM::OECEvent*>(cur->getHeader("/Event/OEC")->event("JM::OECEvent"));
+    auto& t0 = evt->getTime();
+
+    long ientry = entry;
+    while (--ientry >= 0)
+    {
+        if (ientry > m_lastEntry)
+        { //要读的事例在curEntry之后，说明不曾被放入过fragment
+            m_dataStream->prev();
+            auto p = m_dataStream->get();
+            if (!inTimeWindow(p, t0))
+            {
+                delete p;
+                break;
+            }
+            dest->evtDeque.push_front(std::shared_ptr<JM::EvtNavigator>(p));
+        }
+        else
+        { //读入的事例曾被放入过fragment
+            auto _it = m_lastFrag->evtDeque.rbegin() + (m_lastEntry - ientry); //要取的evt在lastFrag的位置
+            if (_it >= m_lastFrag->evtDeque.rend() || !inTimeWindow(_it->get(), t0))
+            {
+                break; //说明向前读的事例已经超出了上一个frag的第0个，或已不在时间窗口内
+            }
+            dest->evtDeque.push_front(*_it);
+        }
+    }
+    dest->lend = dest->evtDeque.size();
+    dest->lbegin = dest->lend - 1;
+
+    ientry = entry;
+    while (++ientry < m_total)
+    {
+        if (ientry > m_lastEntry)
+        { //要读的事例在lastEntry之后,说明不曾被放入过frag
+            m_dataStream->next();
+            auto p = m_dataStream->get();
+            if (!inTimeWindow(p, t0))
+            {
+                delete p;
+                m_lastEntry = ientry - 1; //本次pack结束后更新m_lastEntry
+                break;
+            }
+            dest->evtDeque.push_back(std::shared_ptr<JM::EvtNavigator>(p));
+        }
+        else
+        { //要读的事例在lastEntry之前，需要从lastFrag取
+            auto _it = m_lastFrag->evtDeque.rbegin() + (m_lastEntry - ientry);
+            if (!inTimeWindow(_it->get(), t0))
+            {
+                break;
+            }
+            dest->evtDeque.push_back(*_it);
+        }
+    }
+
+    return dest;
+}
+
+bool PackIndexAlg::inTimeWindow(JM::EvtNavigator *p, const TTimeStamp &t0)
+{
     auto evt = dynamic_cast<JM::OECEvent *>(p->getHeader("/Event/OEC")->event("JM::OECEvent"));
     auto t = evt->getTime();
     t.Add(TTimeStamp(-t0.GetSec(), -t0.GetNanoSec()));
-    if(m_window.first <= t.AsDouble() && t.AsDouble() < m_window.second) 
-        return true;
-    
-    return false;
+    return (m_window.first <= t.AsDouble() && t.AsDouble() < m_window.second);
 }
